b920110e3b88dacb69dffd6eda1d6d7ccd0d1afe
Daco <daco.chen@gmail.com>
2017-07-01 11:24:48 +0800

    初始化feature,steps,pageobjects

---
+++ GOOS_SampleTests/pageobjects/BudgetCreatePage.cs
@@ -0,0 +1,32 @@
+﻿using FluentAutomation;
+using GOOS_SampleTests.steps;
+
+namespace GOOS_SampleTests.pageobjects
+{
+    internal class BudgetCreatePage: PageObject<BudgetCreatePage>
+    {
+        public BudgetCreatePage(FluentTest test) : base(test)
+        {
+        }
+
+        public BudgetCreatePage Amount(int amount)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public BudgetCreatePage Month(string yearMonth)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddBudget()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ShouldDisplay(string message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
